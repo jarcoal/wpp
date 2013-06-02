678ae3511853ff67e629c8ff798244eccb7cda23
Jared <jarcoal@gmail.com>
2013-06-02 16:23:42 -0700

    change output format for get_companies

--- lua/get_companies.lua
+++ lua/get_companies.lua
@@ -1,9 +1,16 @@
 local companiesList = redis.call('zrange', 'companies', 0, -1)
 local companiesData = {}
 
-for idx, companyID in pairs(companiesList) do
+for companyIdx, companyID in pairs(companiesList) do
 	local companyData = redis.call('hgetall', 'companies:'..companyID)
-	companiesData[idx] = companyData
+
+	for dataIdx, data in pairs(companyData) do
+		redis.log(redis.LOG_WARNING, dataIdx, dataIdx % 2)
+
+		if dataIdx % 2 == 0 then
+			table.insert(companiesData, data)
+		end
+	end
 end
 
 return companiesData
